c2ec23f18ff01143779e970e8eee32e0c8438a7d
ks174798 <dy174798@gmail.com>
2025-08-15 07:39:32 +0800

    test(jest): transpile react-native-vector-icons in tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: 'react-native',
   setupFiles: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-native-community)/)'
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-native-community|react-native-vector-icons)/)'
   ],
 };
