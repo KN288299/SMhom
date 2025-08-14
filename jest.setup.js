ee5e4379dbc4b70ea86eca0984eb51d78f8933fa
ks174798 <dy174798@gmail.com>
2025-08-15 07:38:56 +0800

    test(jest): mock RN TurboModules (SettingsManager) & NativeEventEmitter

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,29 @@
 // Mock AsyncStorage for Jest environment
-jest.mock('@react-native-async-storage/async-storage', () => require('@react-native-async-storage/async-storage/jest/async-storage-mock'));
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
 
 // Mock React Native Alert to avoid native calls in tests
 jest.mock('react-native/Libraries/Alert/Alert', () => ({
   alert: jest.fn(),
+}));
+
+// Mock NativeEventEmitter to avoid requiring addListener/removeListeners on native modules
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter', () => {
+  return jest.fn().mockImplementation(() => ({
+    addListener: jest.fn(),
+    removeAllListeners: jest.fn(),
+    removeSubscription: jest.fn(),
+  }));
+});
+
+// Mock NativeSettingsManager which is accessed via TurboModuleRegistry
+jest.mock('react-native/Libraries/Settings/NativeSettingsManager', () => ({
+  getConstants: () => ({
+    AppleInterfaceStyle: 'Light',
+    settings: {},
+    isHDRSupported: false,
+  }),
 }));
 
 // Basic mocks for React Native native modules which might be referenced
@@ -13,6 +33,16 @@ jest.mock('react-native', () => {
     ...RN,
     Platform: { OS: 'ios', select: (obj) => obj.ios },
     StatusBar: RN.View,
+    NativeModules: {
+      ...RN.NativeModules,
+      SettingsManager: {
+        getConstants: () => ({ AppleInterfaceStyle: 'Light', settings: {} }),
+      },
+      DevSettings: {
+        addListener: jest.fn(),
+        removeListeners: jest.fn(),
+      },
+    },
   };
 });
 
