c983143388e1ff96b684f19acbff4277c395fa93
ks174798 <dy174798@gmail.com>
2025-08-18 11:49:45 +0800

    fix: 修复react-native-vector-icons配置问题，移除不支持的sourceDir配置

--- react-native.config.js
+++ react-native.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  dependencies: {
-    'react-native-vector-icons': {
-      platforms: {
-        ios: {
-          sourceDir: '../node_modules/react-native-vector-icons/ios',
-          project: 'RNVectorIcons.xcodeproj',
-        },
-      },
-    },
-  },
   assets: ['./assets/fonts/', './node_modules/react-native-vector-icons/Fonts/'],
 };
