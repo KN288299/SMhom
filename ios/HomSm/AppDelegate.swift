afc47bd718e1a1191d8a354dc52b2525dc080db4
ks174798 <dy174798@gmail.com>
2025-08-23 22:51:07 +0800

    iOS rename: HomeServiceChat -> HomSm (targets, scheme, folder, pods paths)

---
+++ ios/HomSm/AppDelegate.swift
@@ -0,0 +1,24 @@
+import UIKit
+import React
+import React_RCTAppDelegate
+
+
+@UIApplicationMain
+class AppDelegate: RCTAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    self.moduleName = "YUZUTANGJY"
+    self.initialProps = [:]
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func bundleURL() -> URL? {
+#if DEBUG
+    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+#else
+    return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+#endif
+  }
+}
